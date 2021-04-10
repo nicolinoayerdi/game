4ee6a6f442d7a3925afa1867f8963218b8b4dd70
nayerdi <nicolinoayerdi@gmail.com>
2021-04-10 16:56:22 -0300

    Conexion con API y mapeo de respuesta

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,41 @@
 import './App.css';
 import React from 'react';
 import randomuser from "../api/randomuser";
+import Item from "./Item";
+
+/*let json = {
+	id: 1,
+	login.username: 'nayer30',
+	score: 3,
+	picture.medium: '...',
+	lastUpdate: new Date()
+}*/
 
 class App extends React.Component {
 
+    state = {playersStats: []};
+
     getRandomUser = async () => {
         try {
-            const a = await randomuser.get('');
-            console.log(a);
+            const a = await randomuser.get('', {
+                params: {
+                    page: 1,
+                    results: 10,
+                    inc: "login, picture"
+                }
+            });
+            console.log(a.data.results);
+            let playersStats = a.data.results.map((item, index) => {
+                return {
+                    id: index,
+                    username: item.login.username,
+                    img: item.picture.medium,
+                    score: Math.random() * 100,
+                    lastUpdate: new Date(2020, Math.random() * 10, 3)
+                }
+            });
+            console.log(playersStats);
+            this.setState({playersStats: playersStats});
         } catch (e) {
             console.log('error', e);
         }
@@ -18,8 +46,16 @@ class App extends React.Component {
     }
 
     render() {
+        const playersStats = this.state.playersStats.map(playerStat => {
+            return (
+                <div key={playerStat.id}>{playerStat.username}</div>
+            )
+        })
+
         return (
-            <div>App works</div>
+            <div>
+                {playersStats}
+            </div>
         );
     }
 }
